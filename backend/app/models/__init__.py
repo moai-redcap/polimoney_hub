1b4e8abb4a63f68ec77bcb9d94e41de34fc983d9
Haruki Shimizu <shumizu418128@gmail.com>
2026-01-29 21:03:41 +0900

    backendもってきた

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,4 @@
+# Models package
+from .user import User, Role
+from .political_funds import PoliticalFunds
+from .election_funds import ElectionFunds
---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,4 @@
+# Schemas package
+from .user import *
+from .political_funds import *
+from .election_funds import *
